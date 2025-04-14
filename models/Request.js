f19248a28e0f50a7b266fe253f01df7cd6231d3e
zerorich <167667718+zerorich@users.noreply.github.com>
2025-04-14 20:22:48 +0500

    Delete models directory

--- models/Request.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const requestSchema = new mongoose.Schema({
-  login: { type: String, required: true },
-  pass: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Request', requestSchema);
